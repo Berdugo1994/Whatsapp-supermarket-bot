9c0ba8ebfd532403988245fa25e6978824dc7ca9
berdugo1994 <berdugogo@gmail.com>
2022-02-20 01:02:34 +0200

    config mock

---
+++ config(example).py
@@ -0,0 +1,12 @@
+# File must be named as config.py
+# This is an example file, must replace with real values.
+config = {
+    "twilio": {
+        "TWILIO_ACCOUNT": "ACxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+        "TWILIO_TOKEN": "21xxxxxxxxxxxxxxxxxxxxxxxxx",
+        "TWILIO_NUMBER": 'whatsapp:+14155238886',
+    },
+    "mongo":{
+        "MongoClient": 'mongodb+srv://<dbUserName>:<dbPassword>@<clusterName>.ylzij.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+    }
+}
